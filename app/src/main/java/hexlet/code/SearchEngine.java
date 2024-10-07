b25ea5d712286fa083729682a360b0d4b65b4b34
DenisShakhurov <disa.sdf86@gmail.com>
2024-10-07 18:57:23 +0700

    fix checkstyle error

--- app/src/main/java/hexlet/code/SearchEngine.java
+++ app/src/main/java/hexlet/code/SearchEngine.java
@@ -1,6 +1,11 @@
 package hexlet.code;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
