57f16a2c99d415d700ed038779daa23b3710fb2d
DenisShakhurov <disa.sdf86@gmail.com>
2024-10-08 12:57:26 +0700

    add method miltisearch in SearchEngineTest.java

--- app/src/test/java/hexlet/code/SearchEngineTest.java
+++ app/src/test/java/hexlet/code/SearchEngineTest.java
@@ -28,4 +28,22 @@ public class SearchEngineTest {
         List<String> actualNull = SearchEngine.search(new ArrayList<Map<String, String>>(), "ol");
         assertEquals(new ArrayList<>(), actualNull);
     }
+
+    @Test
+    public void testMultiWordSearch() {
+        var doc1 = "I can't shoot straight unless I've had a pint!";
+        var doc2 = "Don't shoot shoot shoot that thing at me.";
+        var doc3 = "I'm your shooter.";
+
+        List<Map<String, String>> docs = List.of(
+                Map.of("id", "doc1", "text", doc1),
+                Map.of("id", "doc2", "text", doc2),
+                Map.of("id", "doc3", "text", doc3)
+        );
+
+        List<String> expected = List.of("doc2", "doc1");
+        List<String> actual = SearchEngine.search(docs, "shoot at me");
+
+        assertEquals(expected, actual);
+    }
 }
